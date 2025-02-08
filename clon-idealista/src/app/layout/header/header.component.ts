6b24f1968f0b760a4eec691dda999ca77d63c71d
Sergio <comprastogira@gmail.com>
2025-02-08 19:06:54 +0100

    Desplegable Idioma

--- clon-idealista/src/app/layout/header/header.component.ts
+++ clon-idealista/src/app/layout/header/header.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  dropdownOpen = false;
 
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  changeLanguage(lang: string) {
+    console.log(`Cambiando a: ${lang}`);
+    this.dropdownOpen = false;
+  }
 }
