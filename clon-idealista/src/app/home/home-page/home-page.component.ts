7bcb444c0b976cd7a890aa314b28f4f8bf14484a
Sergio <comprastogira@gmail.com>
2025-02-19 11:57:21 +0100

    Mensaje aleatorio de logeo

--- clon-idealista/src/app/home/home-page/home-page.component.ts
+++ clon-idealista/src/app/home/home-page/home-page.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home-page', // Asegúrate de que sea este selector
+  selector: 'app-home-page', 
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  // Lista de textos aleatorios para el <h1>
+  mensajes: string[] = [
+    "Necesito un jardín",
+    "Quiero mi casa ideal",
+    "Busco un hogar acogedor",
+    "Mi próximo apartamento me espera",
+    "Encuentra tu lugar soñado",
+    "Tu futuro hogar está aquí"
+  ];
+
+  // Selecciona un mensaje aleatorio al recargar la página
+  mensajeAleatorio: string = this.mensajes[Math.floor(Math.random() * this.mensajes.length)];
+}
