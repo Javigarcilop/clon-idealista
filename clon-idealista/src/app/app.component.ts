a787693a96badc2d14a2e2d61ec6efa2e1f38bea
Javigarcilop <110486658+Javigarcilop@users.noreply.github.com>
2025-02-20 10:15:19 +0100

    commit

--- clon-idealista/src/app/app.component.ts
+++ clon-idealista/src/app/app.component.ts
@@ -1,24 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from './services/api.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'clon-idealista';
-  propiedades: any[] = [];
-
-  constructor(private apiService: ApiService) {}
-
-  ngOnInit() {
-    // Cargar las propiedades al iniciar el componente
-    this.apiService.getPropiedades().subscribe(data => {
-      this.propiedades = data;
-      console.log(this.propiedades); // Para verificar que los datos se cargan correctamente
-    });
-  }
 }
 
  
