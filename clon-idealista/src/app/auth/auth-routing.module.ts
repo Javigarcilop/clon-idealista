c12d14f2a0f81290a1145616c7292c57aba604bb
Javigarcilop <110486658+Javigarcilop@users.noreply.github.com>
2025-02-26 11:35:10 +0100

    commit

--- clon-idealista/src/app/auth/auth-routing.module.ts
+++ clon-idealista/src/app/auth/auth-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent } // 👈 Agregar la ruta para LoginComponent
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class AuthRoutingModule { }
+
