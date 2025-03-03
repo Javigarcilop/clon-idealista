e5ae4dce383c93a75f834e098b3b4dcd07036d39
Javigarcilop <110486658+Javigarcilop@users.noreply.github.com>
2025-03-03 11:55:47 +0100

    commit

--- clon-idealista/src/app/auth/login/login.component.ts
+++ clon-idealista/src/app/auth/login/login.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor() {}
+
+  onSubmit() {
+    console.log('Formulario enviado');
+    console.log('Email:', this.email);
+    console.log('Contraseña:', this.password);
+
+    // Aquí puedes añadir la lógica para manejar el inicio de sesión, como la validación o enviar los datos al backend.
+  }
 }
+
